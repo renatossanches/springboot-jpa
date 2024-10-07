1425710b13950c124fd0358d3b1425812784984c
renatossanches <renato.sanches2004@gmail.com>
2024-10-07 15:39:22 -0300

    Subtotal & Total methods

--- src/main/java/com/springjava/course/entities/Order.java
+++ src/main/java/com/springjava/course/entities/Order.java
@@ -108,6 +108,14 @@ public class Order implements Serializable{
 		return Objects.hash(Id);
 	}
 
+	public Double getTotal() {
+		Double sum = 0.0;
+		for(OrderItem x: items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
